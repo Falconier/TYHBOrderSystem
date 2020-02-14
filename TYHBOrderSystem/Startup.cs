4405a96055a99dbf486577a74e85df0004d91392
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2020-02-13 20:16:39 -0500

    Add project files.

---
+++ TYHBOrderSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TYHBOrderSystem.Startup))]
+namespace TYHBOrderSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
