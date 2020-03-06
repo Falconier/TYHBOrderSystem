5edef27b38f68c2cb3bbbc74b9a46d9d5234f9f6
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2020-03-05 23:49:54 -0500

    Mar3LateNight2
    - scaffolded Products for new product entry and product list manipulation
    - **MODIFIED THE WEB CONFIG FOR SCAFFOLDING PURPOSES**

---
+++ TYHBOrderSystem/Views/Edit/ProductsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TYHBOrderSystem.Models;
+
+namespace TYHBOrderSystem.Views.Edit
+{
+    public class ProductsController : Controller
+    {
+        private BakeryModels db = new BakeryModels();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.PRODUCTS.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            if (pRODUCT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Product_ID,Product_Type,Product_Flavor,Product_Description")] PRODUCT pRODUCT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PRODUCTS.Add(pRODUCT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pRODUCT);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            if (pRODUCT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Product_ID,Product_Type,Product_Flavor,Product_Description")] PRODUCT pRODUCT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pRODUCT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pRODUCT);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            if (pRODUCT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            db.PRODUCTS.Remove(pRODUCT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
