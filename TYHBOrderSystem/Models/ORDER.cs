ac7624308be9139dbfc9585838c0fad650adea34
forgottendrifter <61034424+forgottendrifter@users.noreply.github.com>
2020-03-11 14:23:24 -0400

    Commit3_11_230PM
    Commit of Order.CS and Order_sizes.cs
    for 'recreation' of DB

--- TYHBOrderSystem/Models/ORDER.cs
+++ TYHBOrderSystem/Models/ORDER.cs
@@ -9,44 +9,37 @@ namespace TYHBOrderSystem.Models
     [Table("ORDERS")]
     public partial class ORDER
     {
-        [Key]
+        //PK
         public int ORDER_ID { get; set; }
 
+        //FK
         public int? Customer_ID { get; set; }
 
-        [Required]
-        [StringLength(15)]
+
+        //WIll USE JS to filter date accordingly
         public string Order_Date { get; set; }
 
-        [Required]
-        [StringLength(6)]
-        public string Order_Time { get; set; }
+        //Time (AM/PM)of pickup of Order
+        public string ?Order_Time { get; set; }
 
-        [Required]
-        [StringLength(15)]
-        public string Due_Date { get; set; }
+       //Date of Pickup Order
+        public string PickUp_Due_Date { get; set; }
 
-        public int Product_ID { get; set; }
+        //Time AM/PM of pickup Time
+        public string PickUp_Time { get; set; }
 
-        [StringLength(50)]
-        public string Allergy_Description { get; set; }
+        //FK (FROM Products, Also on Order_Size)
+        public virtual int Product_Type_ID { get; set; }
+       
+        //Keeping for now in case of use of 'ingredients' for population'
+        public string Ingredient_Substitution { get; set; }
 
-        public int? Ingredient_Substitution { get; set; }
+        //Per Jacob, for report views to make easier
+        public string  Decoration_Comments { get; set; }
 
-        public byte[] Decoration_Comments { get; set; }
-
+        //Comments
         public string Additional_Comments { get; set; }
 
-        [StringLength(150)]
-        public string Finishing_Comments { get; set; }
-
-        [Required]
-        [StringLength(15)]
-        public string Pick_Up_Date { get; set; }
-
-        [Required]
-        [StringLength(6)]
-        public string Pick_Up_Time { get; set; }
 
         public int Employee_ID { get; set; }
 
