85508b5ed112ee331a6cc18db2a644d030c011d5
HeyItsVanilla <itsthevanilla@gmail.com>
2020-03-11 21:59:04 -0500

    Joe's Shenanagans

---
+++ TYHBOrderSystem/Controllers/ReportsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TYHBOrderSystem.Models;
+
+namespace TYHBOrderSystem.Controllers
+{
+    public class ReportsController : Controller
+    {
+        ApplicationDbContext DB = new ApplicationDbContext();
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reports/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult DailyReport()
+        {
+            return View();
+        }
+        public ActionResult WeeklyReport()
+        {
+            return View();
+        }
+
+        // POST: Reports/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reports/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reports/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reports/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
