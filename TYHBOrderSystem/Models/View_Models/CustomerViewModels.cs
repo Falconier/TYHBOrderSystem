654e4f0b5d83a668b490775c339b979fd21a9ee3
forgottendrifter <61034424+forgottendrifter@users.noreply.github.com>
2020-04-03 16:12:16 -0400

    ViewUpdateCustomerPage_CW_4_3
    Update View of Customer Information page. In progress of Validation

---
+++ TYHBOrderSystem/Models/View_Models/CustomerViewModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TYHBOrderSystem.Models.View_Models
+{
+    public class CustomerViewModels
+    {
+        [Required(ErrorMessage ="First Name is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is Required")]
+        public string LastName { get; set; }
+
+        public string PrimarymNum { get; set; }
+
+        public string AlternativeNum { get; set; }
+
+        public string AllergyDesc { get; set; }
+
+
+    }
+}
